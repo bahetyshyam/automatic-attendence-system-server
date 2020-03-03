101af45fb59f2fff001787590c9cdab26eae56a6
Shyam Bahety <bahetyshyam@gmail.com>
2020-03-03 21:48:21 +0530

    Set up a simple server on port 3000

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
+app.use(cors());
+
+
+app.listen(3000, () => {
+    console.log("Server Listening on port 3000");
+})
